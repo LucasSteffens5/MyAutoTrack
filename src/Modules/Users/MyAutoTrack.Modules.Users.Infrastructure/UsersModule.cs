44e32f476f8b5c0f0b07e5413aa912dd72e7ff4e
Lucas Steffens <lucassteffensoliveira@hotmail.com>
2024-11-21 13:07:50 -0400

    feat: create assembly references

---
+++ src/Modules/Users/MyAutoTrack.Modules.Users.Infrastructure/UsersModule.cs
@@ -0,0 +1,6 @@
+namespace MyAutoTrack.Modules.Users.Infrastructure;
+
+public static class UsersModule
+{
+    
+}
---
+++ src/Modules/Users/MyAutoTrack.Modules.Users.Presentation/PresentationUsersAssemblyReference.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using System.Reflection.Metadata;
+
+namespace Presentation;
+
+public class PresentationUsersAssemblyReference
+{
+    public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
+}
