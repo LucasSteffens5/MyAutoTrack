9eeacfd9646bc9d525380fe21f55747b8d3a7fd2
Lucas Steffens <lucassteffensoliveira@hotmail.com>
2024-11-21 10:21:02 -0400

    feat: create a global claims extensions

---
+++ src/Common/MyAutoTrack.Common.Infrastructure/Authorization/CustomClaimsTransformation.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+using MyAutoTrack.Common.Application.Authorization;
+using MyAutoTrack.Common.Application.Exceptions;
+using MyAutoTrack.Common.Domain;
+using MyAutoTrack.Common.Infrastructure.Authentication;
+
+namespace MyAutoTrack.Common.Infrastructure.Authorization;
+
+internal sealed class CustomClaimsTransformation(IServiceScopeFactory serviceScopeFactory) : IClaimsTransformation
+{
+    public async Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
+    {
+        if (principal.HasClaim(c => c.Type == CustomClaims.Sub))
+        {
+            return principal;
+        }
+
+        using IServiceScope scope = serviceScopeFactory.CreateScope();
+
+        IPermissionService permissionService = scope.ServiceProvider.GetRequiredService<IPermissionService>();
+
+        string identityId = principal.GetIdentityId();
+
+        Result<PermissionsResponse> result = await permissionService.GetUserPermissionsAsync(identityId);
+
+        if (result.IsFailure)
+        {
+            throw new MyAutoTrackException(nameof(IPermissionService.GetUserPermissionsAsync), result.Error);
+        }
+
+        var claimsIdentity = new ClaimsIdentity();
+
+        claimsIdentity.AddClaim(new Claim(CustomClaims.Sub, result.Value.UserId.ToString()));
+
+        foreach (string permission in result.Value.Permissions)
+        {
+            claimsIdentity.AddClaim(new Claim(CustomClaims.Permission, permission));
+        }
+
+        principal.AddIdentity(claimsIdentity);
+
+        return principal;
+    }
+}
