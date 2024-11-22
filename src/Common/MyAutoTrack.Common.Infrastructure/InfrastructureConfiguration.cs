d63ca9453592d7fb9978fc07ecd94b4668116b49
Lucas Steffens <lucassteffensoliveira@hotmail.com>
2024-11-22 11:02:54 -0400

    feat: create InfrastructureConfiguration common

---
+++ src/Common/MyAutoTrack.Common.Infrastructure/InfrastructureConfiguration.cs
@@ -0,0 +1,115 @@
+﻿using Dapper;
+using MassTransit;
+using MassTransit.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using MyAutoTrack.Common.Application.Caching;
+using MyAutoTrack.Common.Application.Clock;
+using MyAutoTrack.Common.Application.Data;
+using MyAutoTrack.Common.Application.EventBus;
+using MyAutoTrack.Common.Infrastructure.Authentication;
+using MyAutoTrack.Common.Infrastructure.Authorization;
+using MyAutoTrack.Common.Infrastructure.Caching;
+using MyAutoTrack.Common.Infrastructure.Clock;
+using MyAutoTrack.Common.Infrastructure.Data;
+using MyAutoTrack.Common.Infrastructure.EventBus;
+using MyAutoTrack.Common.Infrastructure.Outbox;
+using Npgsql;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using Quartz;
+using StackExchange.Redis;
+
+namespace MyAutoTrack.Common.Infrastructure;
+
+public static class InfrastructureConfiguration
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        string serviceName,
+        Action<IRegistrationConfigurator, string>[] moduleConfigureConsumers,
+        RabbitMqSettings rabbitMqSettings,
+        string databaseConnectionString,
+        string redisConnectionString)
+    {
+        services.AddAuthenticationInternal();
+
+        services.AddAuthorizationInternal();
+
+        services.TryAddSingleton<IDateTimeProvider, DateTimeProvider>();
+
+        services.TryAddSingleton<IEventBus, EventBus.EventBus>();
+
+        services.TryAddSingleton<InsertOutboxMessagesInterceptor>();
+
+        NpgsqlDataSource npgsqlDataSource = new NpgsqlDataSourceBuilder(databaseConnectionString).Build();
+        services.TryAddSingleton(npgsqlDataSource);
+
+        services.TryAddScoped<IDbConnectionFactory, DbConnectionFactory>();
+
+        SqlMapper.AddTypeHandler(new GenericArrayHandler<string>());
+
+        services.AddQuartz(configurator =>
+        {
+            var scheduler = Guid.NewGuid();
+            configurator.SchedulerId = $"default-id-{scheduler}";
+            configurator.SchedulerName = $"default-name-{scheduler}";
+        });
+
+        services.AddQuartzHostedService(options => options.WaitForJobsToComplete = true);
+
+        try
+        {
+            IConnectionMultiplexer connectionMultiplexer = ConnectionMultiplexer.Connect(redisConnectionString);
+            services.AddSingleton(connectionMultiplexer);
+            services.AddStackExchangeRedisCache(options =>
+                options.ConnectionMultiplexerFactory = () => Task.FromResult(connectionMultiplexer));
+        }
+        catch
+        {
+            services.AddDistributedMemoryCache();
+        }
+
+        services.TryAddSingleton<ICacheService, CacheService>();
+
+        services.AddMassTransit(configure =>
+        {
+            string instanceId = serviceName.ToLowerInvariant().Replace('.', '-');
+            foreach (Action<IRegistrationConfigurator, string> configureConsumers in moduleConfigureConsumers)
+            {
+                configureConsumers(configure, instanceId);
+            }
+
+            configure.SetKebabCaseEndpointNameFormatter();
+
+            configure.UsingRabbitMq((context, cfg) =>
+            {
+                cfg.Host(new Uri(rabbitMqSettings.Host), h =>
+                {
+                    h.Username(rabbitMqSettings.Username);
+                    h.Password(rabbitMqSettings.Password);
+                });
+
+                cfg.ConfigureEndpoints(context);
+            });
+        });
+
+        services
+            .AddOpenTelemetry()
+            .ConfigureResource(resource => resource.AddService(serviceName))
+            .WithTracing(tracing =>
+            {
+                tracing
+                    .AddAspNetCoreInstrumentation()
+                    .AddHttpClientInstrumentation()
+                    .AddEntityFrameworkCoreInstrumentation()
+                    .AddRedisInstrumentation()
+                    .AddNpgsql()
+                    .AddSource(DiagnosticHeaders.DefaultListenerName);
+
+                tracing.AddOtlpExporter();
+            });
+
+        return services;
+    }
+}
