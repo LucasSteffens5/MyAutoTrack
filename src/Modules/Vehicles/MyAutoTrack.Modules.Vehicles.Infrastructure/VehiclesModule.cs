c335ab410fe61904aa7295b986171f7579632e45
Lucas Steffens <lucassteffensoliveira@hotmail.com>
2024-11-26 16:03:46 -0400

    feat: configure dependencies to module vehicles

--- src/Modules/Vehicles/MyAutoTrack.Modules.Vehicles.Infrastructure/VehiclesModule.cs
+++ src/Modules/Vehicles/MyAutoTrack.Modules.Vehicles.Infrastructure/VehiclesModule.cs
@@ -1,6 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyAutoTrack.Modules.Vehicles.Application.Abstractions.Data;
+using MyAutoTrack.Modules.Vehicles.Infrastructure.Database;
+
 namespace MyAutoTrack.Modules.Vehicles.Infrastructure;
 
-public class VehiclesModule
+public static class VehiclesModule
 {
-    
+    public static IServiceCollection AddVehiclesModule(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddInfrastructure(configuration);
+
+        return services;
+    }
+
+    private static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<VehiclesDbContext>((sp, options) =>
+            options
+                .UseNpgsql(
+                    configuration.GetConnectionString("Database"),
+                    npgsqlOptions => npgsqlOptions
+                        .MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Vehicles))
+                .UseSnakeCaseNamingConvention());
+
+        services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<VehiclesDbContext>());
+    }
 }
