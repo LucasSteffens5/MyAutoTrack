2987d062b3af0745e9461b220a1a24b41ecb3b13
Lucas Steffens <lucassteffensoliveira@hotmail.com>
2024-11-27 16:40:27 -0400

    feat: implement vehicle repository

---
+++ src/Modules/Vehicles/MyAutoTrack.Modules.Vehicles.Infrastructure/Vehicles/VehicleRepository.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using MyAutoTrack.Modules.Vehicles.Domain.Vehicles;
+using MyAutoTrack.Modules.Vehicles.Infrastructure.Database;
+
+namespace MyAutoTrack.Modules.Vehicles.Infrastructure.Vehicles;
+
+internal sealed class VehicleRepository(VehiclesDbContext context) : IVehicleRepository
+{
+    public async Task<Vehicle?> GetAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        return await context.Vehicles.SingleOrDefaultAsync(v => v.Id == id, cancellationToken);
+    }
+
+    public void Insert(Vehicle vehicle)
+    {
+        context.Vehicles.Add(vehicle);
+    }
+}
