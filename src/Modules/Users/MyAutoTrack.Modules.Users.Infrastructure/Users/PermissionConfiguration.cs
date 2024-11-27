06b8b89c9086b77cc02ecc6a6b4cbd942a497cc5
Lucas Steffens <lucassteffensoliveira@hotmail.com>
2024-11-27 17:31:13 -0400

    feat: create permission vehicles:update and expose endpoint

--- src/Modules/Users/MyAutoTrack.Modules.Users.Infrastructure/Users/PermissionConfiguration.cs
+++ src/Modules/Users/MyAutoTrack.Modules.Users.Infrastructure/Users/PermissionConfiguration.cs
@@ -16,7 +16,8 @@ internal sealed class PermissionConfiguration : IEntityTypeConfiguration<Permiss
 
         builder.HasData(
             Permission.GetUser,
-            Permission.ModifyUser); // TODO Aqui adicioanr mais permissoes para outros modulos
+            Permission.ModifyUser,
+            Permission.ModifyVehicles); // TODO Aqui adicioanr mais permissoes para outros modulos
 
         builder
             .HasMany<Role>()
@@ -28,10 +29,13 @@ internal sealed class PermissionConfiguration : IEntityTypeConfiguration<Permiss
                 joinBuilder.HasData(
                     // Member permissions
                     CreateRolePermission(Role.Member, Permission.GetUser),
+                    CreateRolePermission(Role.Member, Permission.ModifyVehicles),
                   
                     // Admin permissions
-                    CreateRolePermission(Role.Administrator, Permission.GetUser), //TODO Aqui adicionar mais roles para outros modulos
-                    CreateRolePermission(Role.Administrator, Permission.ModifyUser));
+                    CreateRolePermission(Role.Administrator, Permission.GetUser), 
+                    CreateRolePermission(Role.Administrator, Permission.ModifyUser),
+                    CreateRolePermission(Role.Administrator, Permission.ModifyVehicles) //TODO Aqui adicionar mais roles para outros modulos
+                    );
             });
     }
 
