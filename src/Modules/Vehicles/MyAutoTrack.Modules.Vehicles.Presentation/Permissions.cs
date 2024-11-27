72ce42802652396b376241d9840c6508133b68f7
Lucas Steffens <lucassteffensoliveira@hotmail.com>
2024-11-27 17:31:46 -0400

    feat: remove todo

--- src/Modules/Vehicles/MyAutoTrack.Modules.Vehicles.Presentation/Permissions.cs
+++ src/Modules/Vehicles/MyAutoTrack.Modules.Vehicles.Presentation/Permissions.cs
@@ -2,5 +2,5 @@ namespace MyAutoTrack.Modules.Vehicles.Presentation;
 
 internal static class Permissions
 {
-    internal const string ModifyVehicles = "vehicles:update"; // TODO: Verificar todos pontos para criar esta permissão
+    internal const string ModifyVehicles = "vehicles:update";
 }
