4561fbdbf0a781eb9a9dc3fe529c8aa908bd9a08
Lucas Steffens <lucassteffensoliveira@hotmail.com>
2024-11-27 16:49:58 -0400

    feat: implement Owner repository

---
+++ src/Modules/Vehicles/MyAutoTrack.Modules.Vehicles.Domain/Owners/IOwnersRepository.cs
@@ -0,0 +1,8 @@
+namespace MyAutoTrack.Modules.Vehicles.Domain.Owners;
+
+public interface IOwnersRepository
+{
+    Task<Owner?> GetAsync(Guid id, CancellationToken cancellationToken = default);
+
+    void Insert(Owner owner);
+}
---
+++ src/Modules/Vehicles/MyAutoTrack.Modules.Vehicles.Infrastructure/Owners/OwnersRepository.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using MyAutoTrack.Modules.Vehicles.Domain.Owners;
+using MyAutoTrack.Modules.Vehicles.Infrastructure.Database;
+
+namespace MyAutoTrack.Modules.Vehicles.Infrastructure.Owners;
+
+public class OwnersRepository(VehiclesDbContext context) : IOwnersRepository
+{
+    public async Task<Owner?> GetAsync(Guid id, CancellationToken cancellationToken = default)
+        => await context.Owners.SingleOrDefaultAsync(v => v.Id == id, cancellationToken);
+
+
+    public void Insert(Owner owner)
+    {
+        context.Owners.Add(owner);
+    }
+}
