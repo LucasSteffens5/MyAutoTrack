7902e91d2a8ddd16e90870b173850ec1bcc336c0
Lucas Steffens <lucassteffensoliveira@hotmail.com>
2024-12-07 11:49:52 -0400

    feat: add permission get to presentation layer

--- src/Modules/Vehicles/MyAutoTrack.Modules.Vehicles.Presentation/Permissions.cs
+++ src/Modules/Vehicles/MyAutoTrack.Modules.Vehicles.Presentation/Permissions.cs
@@ -3,4 +3,5 @@ namespace MyAutoTrack.Modules.Vehicles.Presentation;
 internal static class Permissions
 {
     internal const string ModifyVehicles = "vehicles:update";
+    internal const string GetVehicles = "vehicles:read";
 }
