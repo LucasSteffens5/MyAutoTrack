0d66419e5c65181d9fb2f0e66f374e978d234f93
Lucas Steffens <lucassteffensoliveira@hotmail.com>
2024-12-09 20:31:27 -0400

    feat: create migrations

---
+++ src/Modules/Maintenance/MyAutoTrack.Modules.Maintenance.Infrastructure/Database/Migrations/20241210002234_Create_Initial_Entities_Module_Maintenance.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyAutoTrack.Modules.Maintenance.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Create_Initial_Entities_Module_Maintenance : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "maintenance");
+
+            migrationBuilder.CreateTable(
+                name: "items",
+                schema: "maintenance",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    type = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    price = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    inventory = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_items", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "vehicles",
+                schema: "maintenance",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    description = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    fabrication_year = table.Column<int>(type: "integer", nullable: false),
+                    mileage = table.Column<long>(type: "bigint", nullable: false),
+                    license_plate = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    owner_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    manufacturer_id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_vehicles", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "maintenances",
+                schema: "maintenance",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    vehicle_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    total_price = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    status = table.Column<int>(type: "integer", nullable: false),
+                    starts_at_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ends_at_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    mileage = table.Column<long>(type: "bigint", nullable: false),
+                    description = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_maintenances", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_maintenances_vehicles_vehicle_id",
+                        column: x => x.vehicle_id,
+                        principalSchema: "maintenance",
+                        principalTable: "vehicles",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "maintenance_items",
+                schema: "maintenance",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    maintenance_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    item_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    price = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    quantity = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_maintenance_items", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_maintenance_items_items_item_id",
+                        column: x => x.item_id,
+                        principalSchema: "maintenance",
+                        principalTable: "items",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_maintenance_items_maintenances_maintenance_id",
+                        column: x => x.maintenance_id,
+                        principalSchema: "maintenance",
+                        principalTable: "maintenances",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_maintenance_items_item_id",
+                schema: "maintenance",
+                table: "maintenance_items",
+                column: "item_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_maintenance_items_maintenance_id",
+                schema: "maintenance",
+                table: "maintenance_items",
+                column: "maintenance_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_maintenances_vehicle_id",
+                schema: "maintenance",
+                table: "maintenances",
+                column: "vehicle_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "maintenance_items",
+                schema: "maintenance");
+
+            migrationBuilder.DropTable(
+                name: "items",
+                schema: "maintenance");
+
+            migrationBuilder.DropTable(
+                name: "maintenances",
+                schema: "maintenance");
+
+            migrationBuilder.DropTable(
+                name: "vehicles",
+                schema: "maintenance");
+        }
+    }
+}
