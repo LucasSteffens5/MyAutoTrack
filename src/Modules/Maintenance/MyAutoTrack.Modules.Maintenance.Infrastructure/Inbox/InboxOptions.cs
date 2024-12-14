9918a3eded2eb1050540d8523a5c8b2ca93bcc75
Lucas Steffens <lucassteffensoliveira@hotmail.com>
2024-12-14 10:30:32 -0400

    feat: implement inbox and outbox to module Maintenance

---
+++ src/Modules/Maintenance/MyAutoTrack.Modules.Maintenance.Infrastructure/Inbox/InboxOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace MyAutoTrack.Modules.Maintenance.Infrastructure.Inbox;
+
+internal sealed class InboxOptions
+{
+    public int IntervalInSeconds { get; init; }
+
+    public int BatchSize { get; init; }
+}
